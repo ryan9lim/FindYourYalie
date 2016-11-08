35cb2db00e9823084bcc606441596020679434f5
Ryan Lim <ryan9lim@gmail.com>
2016-11-08 03:45:42 -0500

    Added sign up to backend

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,38 @@ app.use(passport.initialize());
 app.get('/', function(req, res) {
   res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
- 
+
 // Start the server
 app.listen(port);
 console.log('There will be dragons: http://localhost:' + port);
+
+// connect to database
+mongoose.connect(config.database);
+ 
+// pass passport for configuration
+require('./config/passport')(passport);
+ 
+// bundle our routes
+var apiRoutes = express.Router();
+ 
+// create a new user account (POST http://localhost:8080/api/signup)
+apiRoutes.post('/signup', function(req, res) {
+  if (!req.body.name || !req.body.password) {
+    res.json({success: false, msg: 'Please pass name and password.'});
+  } else {
+    var newUser = new User({
+      name: req.body.name,
+      password: req.body.password
+    });
+    // save the user
+    newUser.save(function(err) {
+      if (err) {
+        return res.json({success: false, msg: 'Username already exists.'});
+      }
+      res.json({success: true, msg: 'Successful created new user.'});
+    });
+  }
+});
+ 
+// connect the api routes under /api/*
+app.use('/api', apiRoutes);
